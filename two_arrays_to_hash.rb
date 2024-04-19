be0fffccff91d30c9ec6b4e548afb729819b0bf7
Sebastian Olivares <olivares.heredia@gmail.com>
2024-04-19 12:10:04 -0600

    two arrays to hash

---
+++ two_arrays_to_hash.rb
@@ -0,0 +1,16 @@
+# Generate a Hash from Two Arrays
+
+
+
+def hash_generator(ar1, ar2)
+  hash = Hash.new
+  ar1.each_with_index do |x, i|
+    hash[x] = ar2[i]
+  end
+  return hash
+end
+
+categories = ["Name", "Song", "Album"]
+artist = ["Anderson .Paak", "6 Summers", "Oxnard"]
+
+p hash_generator(categories, artist)
